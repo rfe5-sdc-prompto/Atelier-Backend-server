366afdc2232e4f2dc3c75adfc02d2f810c45b0bc
jianzhang <jianzhang0412@gmail.com>
2021-10-05 15:28:20 -0400

    modify database

--- db.js
+++ db.js
@@ -3,9 +3,13 @@ const Pool = require('pg').Pool;
 const pool = new Pool({
   user:'postgres',
   password:'postgres',
-  database: 'rating_reviews',
-  host:'localhost',
+  database: 'awsdb',
+  host:'3.19.238.190',
   port:5432
+})
+
+pool.query('select * from photos where id=1').then(data=>{
+  console.log(data.rows[0].id, 'database is connected')
 })
 
 module.exports = pool;
